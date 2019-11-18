1e4875ca185abb30857ab06189a1ed5b8cf74102
zraso <zaira.rasool@hotmail.co.uk>
2019-11-18 12:02:03 +0000

    Displays float of any given number

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,3 +1,9 @@
 def deposit_display(amount)
-  1000.00
+  if amount == 1000
+    amount.to_f
+  elsif amount == 3000
+    amount.to_f
+  else
+    amount.to_f
+  end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,5 +3,7 @@ require 'bank'
 describe '#deposit_display' do
   it 'displays a given amount in the correct format' do
     expect(deposit_display(1000)).to eq 1000.00
+    expect(deposit_display(2000)).to eq 2000.00
+    expect(deposit_display(3000)).to eq 3000.00
   end
 end
