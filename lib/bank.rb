724d39b175654787ce836e319ff07fac14aa0e95
zraso <zaira.rasool@hotmail.co.uk>
2019-11-18 12:31:05 +0000

    Change output format to string with two decimal places

--- lib/bank.rb
+++ lib/bank.rb
@@ -2,6 +2,6 @@ def deposit_display(amount)
   if amount == -2000
     "Error: invalid amount"
   else 
-    amount.to_f
+    sprintf("%.2f", amount)
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,12 +2,17 @@ require 'bank'
 
 describe '#deposit_display' do
   it 'displays a given amount in the correct format' do
-    expect(deposit_display(1000)).to eq 1000.00
-    expect(deposit_display(2000)).to eq 2000.00
-    expect(deposit_display(3000)).to eq 3000.00
+    expect(deposit_display(1000)).to eq "1000.00"
+    expect(deposit_display(2000)).to eq "2000.00"
+    expect(deposit_display(3000)).to eq "3000.00"
+    expect(deposit_display(100.53)).to eq "100.53"
   end
 
-  it 'throws an if given amount is in incorrect format' do
+  it 'throws an if given amount is an invalid amount' do
     expect(deposit_display(-2000)).to eq "Error: invalid amount"
   end
-end
+
+  # it 'throws an error if given amount is in an incorrect format' do
+  #   expect(deposit_display("2000")).to eq "Error: invalid format"
+  # end
+  end
